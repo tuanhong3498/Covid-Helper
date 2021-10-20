2d3838dd116dc583adc6b5e9616cf87cc9d84e8d
tuanhong3498 <54527027+tuanhong3498@users.noreply.github.com>
2021-10-21 01:59:11 +0800

    added four main fragments and completed the navigation between them

--- app/src/main/java/com/example/covidhelper/MainActivity.java
+++ app/src/main/java/com/example/covidhelper/MainActivity.java
@@ -1,16 +1,47 @@
 package com.example.covidhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.dashboardFragment,
+                R.id.checkInFragment,
+                R.id.announcementFragment,
+                R.id.profileFragment
+        ).build();
+
+        navController = Navigation.findNavController(this, R.id.fragment_container);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp()
+    {
+        findViewById(R.id.bottom_navigation_view).setVisibility(View.VISIBLE);
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
