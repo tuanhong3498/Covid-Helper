d90112037fdb7a9d69888e3a19f9b6b0c0d0f86f
tuanhong3498 <54527027+tuanhong3498@users.noreply.github.com>
2021-10-31 02:13:37 +0800

    Vaccination UI -> added top status icons

---
+++ app/src/main/java/com/example/covidhelper/VaccinationFragment.java
@@ -0,0 +1,40 @@
+package com.example.covidhelper;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VaccinationFragment extends Fragment
+{
+
+    private VaccinationViewModel mViewModel;
+
+    public static VaccinationFragment newInstance()
+    {
+        return new VaccinationFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_vaccination, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(VaccinationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/covidhelper/VaccinationViewModel.java
@@ -0,0 +1,8 @@
+package com.example.covidhelper;
+
+import androidx.lifecycle.ViewModel;
+
+public class VaccinationViewModel extends ViewModel
+{
+    // TODO: Implement the ViewModel
+}
