634e9634369afebbb8f56cf0943f2164928cca03
weilifan <liyixin24@gmail.com>
2021-11-08 15:08:45 +0800

    Complete the check in scan interface

---
+++ app/src/main/java/com/example/covidhelper/ui/checkIn/Capture.java
@@ -0,0 +1,6 @@
+package com.example.covidhelper.ui.checkIn;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+}
--- app/src/main/java/com/example/covidhelper/ui/checkIn/CheckInFragment.java
+++ app/src/main/java/com/example/covidhelper/ui/checkIn/CheckInFragment.java
@@ -1,17 +1,25 @@
 package com.example.covidhelper.ui.checkIn;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.covidhelper.R;
-import com.example.covidhelper.ui.announcement.AnnouncementAdapter;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,12 +27,32 @@ import java.util.List;
 
 public class CheckInFragment extends Fragment implements RecentlyCheckInListAdapter.RecyclerviewOnClickListener
 {
+    Button btScan;
+    TextView btViewAll;
+
     RecyclerView recyclerView;
     List<String> place,address,time;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View root = inflater.inflate(R.layout.fragment_check_in, container, false);
+
+        btScan = root.findViewById(R.id.scan_btn);
+        btViewAll = root.findViewById(R.id.view_all);
+
+        btScan.setOnClickListener(v -> {
+            IntentIntegrator intentIntegrator= IntentIntegrator.forSupportFragment(this);
+            intentIntegrator.setPrompt("For flash use volume up key");
+            intentIntegrator.setBeepEnabled(true);
+            intentIntegrator.setOrientationLocked(true);
+            intentIntegrator.setCaptureActivity(Capture.class);
+            intentIntegrator.initiateScan();
+        });
+        btViewAll.setOnClickListener(v ->
+        {
+            NavController navController = Navigation.findNavController(getActivity(), R.id.fragment_container);
+            navController.navigate(R.id.checkInHistoryFragment);
+        });
 
         recyclerView = root.findViewById(R.id.all_announcements_recyclerview);
         place = new ArrayList<>();
@@ -49,5 +77,22 @@ public class CheckInFragment extends Fragment implements RecentlyCheckInListAdap
     @Override
     public void recyclerviewClick(int position) {
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Result");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("OK", (dialogInterface, which) -> dialogInterface.dismiss());
+            builder.show();
+        }else {
+            Toast.makeText(getActivity().getApplicationContext()
+                    ,"Oops...You did not scan anything,"
+                    , Toast.LENGTH_SHORT).show();
+        }
     }
 }
