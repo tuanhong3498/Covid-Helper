d0d1d91458fbd238d3025a7567e0d6cb3c18ae68
weilifan <liyixin24@gmail.com>
2021-11-11 14:36:19 +0800

    Add Google Map to hotspot interface

--- app/src/main/java/com/example/covidhelper/ui/dashboard/HotspotFragment.java
+++ app/src/main/java/com/example/covidhelper/ui/dashboard/HotspotFragment.java
@@ -1,93 +1,94 @@
 package com.example.covidhelper.ui.dashboard;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
-
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.covidhelper.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
 
-public class HotspotFragment extends Fragment
+public class HotspotFragment extends Fragment implements OnMapReadyCallback
 {
+    GoogleMap map;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_hotspot, container, false);
 
-        GoogleMap map;
         SearchView searchView = root.findViewById(R.id.sv_location);
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.google_map);
-
-
-
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng latLng) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        markerOptions.title(latLng.latitude + ":" + latLng.longitude);
-                        googleMap.clear();
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng,0
-                        ));
-                        googleMap.addMarker(markerOptions);
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList  = null;
+
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(getContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
-//                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//                    @Override
-//                    public boolean onQueryTextSubmit(String query) {
-//                        String location = searchView.getQuery().toString();
-//                        List<Address> addressList  = null;
-//
-//                        if(location != null || !location.equals("")){
-//                            Geocoder geocoder = new Geocoder(getContext());
-//                            try {
-//                                addressList = geocoder.getFromLocationName(location,1);
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                            Address address = addressList.get(0);
-//                            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-//                            googleMap.addMarker(new MarkerOptions().position(latLng).title(location));
-//                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-//                        }
-//
-//                        return false;
-//                    }
-//
-//                    @Override
-//                    public boolean onQueryTextChange(String newText) {
-//                        return false;
-//                    }
-//                });
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        supportMapFragment.getMapAsync(this);
+
         return root;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnMapClickListener(latLng -> {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title(latLng.latitude + ":" + latLng.longitude);
+            map.clear();
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    latLng,10
+            ));
+            map.addMarker(markerOptions);
+        });
+        Circle circle1 = map.addCircle(new CircleOptions()
+                .center(new LatLng(2.8324251,101.7047863))
+                .radius(10000)
+                .strokeColor(Color.parseColor("#D61313"))
+                .fillColor(Color.parseColor("#5EFF1C1C")));
+        Circle circle2 = map.addCircle(new CircleOptions()
+                .center(new LatLng(3.1144229,101.5954963))
+                .radius(10000)
+                .strokeColor(Color.parseColor("#D61313"))
+                .fillColor(Color.parseColor("#5EFF1C1C")));
+    }
 }
