9b6a43a62417fa1d9f99120cc381b68a3af3adc8
tuanhong3498 <54527027+tuanhong3498@users.noreply.github.com>
2021-11-25 05:21:09 +0800

    removed toolbar elevation on AnnouncementFragment

--- app/src/main/java/com/example/covidhelper/MainActivity.java
+++ app/src/main/java/com/example/covidhelper/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.covidhelper;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.animation.ObjectAnimator;
+import android.animation.StateListAnimator;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -16,12 +21,15 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity
 {
     private NavController navController;
+    private StateListAnimator originalAnimator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -49,6 +57,28 @@ public class MainActivity extends AppCompatActivity
         // on top destination, the navigation up button (an arrow pointing to the left) will not be displayed
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar_layout);
+        originalAnimator = appBarLayout.getStateListAnimator();
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener()
+        {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments)
+            {
+                if(destination.getId() == R.id.announcementFragment)
+                {
+                    // remove elevation for announcement fragment
+                    StateListAnimator stateListAnimator = new StateListAnimator();
+                    stateListAnimator.addState(new int[0], ObjectAnimator.ofFloat(appBarLayout, "elevation", 0));
+                    appBarLayout.setStateListAnimator(stateListAnimator);
+                }
+                else
+                {
+                    appBarLayout.setStateListAnimator(originalAnimator);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/covidhelper/ui/announcement/AnnouncementFragment.java
+++ app/src/main/java/com/example/covidhelper/ui/announcement/AnnouncementFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
