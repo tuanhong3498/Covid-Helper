609417035cfcaafafe46d005ea34f05b18b8eede
tuanhong3498 <54527027+tuanhong3498@users.noreply.github.com>
2021-12-02 02:27:28 +0800

    added button navigation to visit NRP's website (to view complete SOPs)

--- app/src/main/java/com/example/covidhelper/ui/dashboard/tools/SOP/SopFragment.java
+++ app/src/main/java/com/example/covidhelper/ui/dashboard/tools/SOP/SopFragment.java
@@ -2,6 +2,8 @@ package com.example.covidhelper.ui.dashboard.tools.SOP;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,14 +17,30 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.covidhelper.R;
+import com.google.android.material.button.MaterialButton;
 
 public class SopFragment extends Fragment
 {
+    // UI elements
+    private AutoCompleteTextView autoCompleteTextViewState;
+    private TextView textViewCurrentPhase;
 
-    AutoCompleteTextView autoCompleteTextViewState;
+    private ImageView statusDineIn;
+    private ImageView statusNonContactSport;
+    private ImageView statusContactSport;
+    private ImageView statusInterDistrictTravel;
+    private ImageView statusInterStateTravel;
+    private ImageView statusExamClass;
+    private ImageView statusNonExamClass;
+    private ImageView statusSocialActivity;
+
+    private MaterialButton buttonCompleteSOP;
 
     private SopViewModel mViewModel;
 
@@ -38,6 +56,18 @@ public class SopFragment extends Fragment
         View root = inflater.inflate(R.layout.fragment_sop, container, false);
 
         autoCompleteTextViewState = root.findViewById(R.id.SOP_autoTextView_state);
+        textViewCurrentPhase = root.findViewById(R.id.SOP_text_phase);
+
+        statusDineIn = root.findViewById(R.id.SOP_dineIn_status);
+        statusNonContactSport = root.findViewById(R.id.SOP_sport_nonContact_status);
+        statusContactSport = root.findViewById(R.id.SOP_sport_contact_status);
+        statusInterDistrictTravel = root.findViewById(R.id.SOP_travel_interDistrict_status);
+        statusInterStateTravel = root.findViewById(R.id.SOP_travel_interState_status);
+        statusExamClass = root.findViewById(R.id.SOP_school_examClass_status);
+        statusNonExamClass = root.findViewById(R.id.SOP_school_nonExamClass_status);
+        statusSocialActivity = root.findViewById(R.id.SOP_socialActivity_status);
+
+        buttonCompleteSOP = root.findViewById(R.id.SOP_button_complete_SOP);
 
         return root;
     }
@@ -48,6 +78,28 @@ public class SopFragment extends Fragment
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SopViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        autoCompleteTextViewState.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+
+            }
+        });
+
+        buttonCompleteSOP.setOnClickListener(v ->
+        {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://pelanpemulihannegara.gov.my/selangor/index-en.html"));
+            startActivity(intent);
+        });
     }
 
     @Override
